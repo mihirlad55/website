52bad6d2961f3c13426d6e634474d9145926b6bf
Mihir Lad <mihirlad55@gmail.com>
2020-08-02 00:20:22 -0400

    Add SkillComponent for representing skills

---
+++ src/app/skill/skill.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {Skill} from '../skills.service';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.css']
+})
+export class SkillComponent implements OnInit {
+  @Input() skill: Skill;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
