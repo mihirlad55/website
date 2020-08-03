2e660dd777fff2cb286103a235dc9186a786993a
Mihir Lad <mihirlad55@gmail.com>
2020-08-02 20:57:04 -0400

    Update skills styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TypewriterComponent } from './typewriter/typewriter.component';
-import { SkillComponent } from './skill/skill.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {TypewriterComponent} from './typewriter/typewriter.component';
+import {SkillComponent} from './skill/skill.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TypewriterComponent,
-    SkillComponent
+    SkillComponent,
+    SkillsSectionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
