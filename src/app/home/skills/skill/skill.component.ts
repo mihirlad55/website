61ff8ef9387c77a79e05dd736b4ab40e1adb9d51
Mihir Lad <mihirlad55@gmail.com>
2020-08-03 23:44:01 -0400

    Move skills.service to folder

--- src/app/home/skills/skill/skill.component.ts
+++ src/app/home/skills/skill/skill.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {Skill} from '../../../services/skills.service';
+import {Skill} from '../../../services/skills/skills.service';
 
 @Component({
   selector: 'app-skill',
--- src/app/home/skills/skills-section.component.ts
+++ src/app/home/skills/skills-section.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {SkillsService, Skill} from '../../services/skills.service';
+import {SkillsService, Skill} from '../../services/skills/skills.service';
 
 @Component({
   selector: 'app-skills-section',
