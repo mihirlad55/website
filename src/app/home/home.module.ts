ab75a6c94ecf5d78d11b9160b393d2f907824152
Mihir Lad <mihirlad55@gmail.com>
2020-08-04 00:18:09 -0400

    Add SharedModule to fix build errors
    TypewriterComponent is exported by this module which can be used by others.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {SharedModule} from '../shared/shared.module';
+
 import {HomeComponent} from './home.component';
 import {HeaderComponent} from './header/header.component';
 import {SkillComponent} from './skills/skill/skill.component';
 import {SkillsSectionComponent} from './skills/skills-section.component';
-import {AppModule} from '../app.module';
 import {IntroSectionComponent} from './intro-section/intro-section.component';
 import {ParallaxDirective} from '../directives/parallax/parallax.directive';
 import {StatsSectionComponent} from './stats-section/stats-section.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,7 @@ import {StatsSectionComponent} from './stats-section/stats-section.component';
   ],
   imports: [
     CommonModule,
-    AppModule
+    SharedModule
   ]
 })
 export class HomeModule {}
