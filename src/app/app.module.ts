e21e5dc2795b89f3c8e697b6fb415bafe9dbe30a
Mihir Lad <mihirlad55@gmail.com>
2020-08-03 23:44:20 -0400

    Format app.module.ts and home.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import {AppRoutingModule} from './app-routing.module';
 
-import {HomeModule} from './home/home.module';
-
 import {AppComponent} from './app.component';
 import {TypewriterComponent} from './shared/typewriter/typewriter.component';
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,9 @@ import {HeaderComponent} from './header/header.component';
 import {SkillComponent} from './skills/skill/skill.component';
 import {SkillsSectionComponent} from './skills/skills-section.component';
 import {AppModule} from '../app.module';
-import { IntroSectionComponent } from './intro-section/intro-section.component';
-import { ParallaxDirective } from '../directives/parallax/parallax.directive';
-import { StatsSectionComponent } from './stats-section/stats-section.component';
+import {IntroSectionComponent} from './intro-section/intro-section.component';
+import {ParallaxDirective} from '../directives/parallax/parallax.directive';
+import {StatsSectionComponent} from './stats-section/stats-section.component';
 
 @NgModule({
   declarations: [
